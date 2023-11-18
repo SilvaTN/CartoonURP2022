21aaee7ffe3c9a58d5152c283801914a972c2d4f
T. Norie <partlytangible@gmail.com>
2023-11-18 13:23:29 +1300

    Initial commit

---
+++ Assets/JammoMixamo/AnimationStateController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateController : MonoBehaviour
+{
+    Animator animator;
+    bool canStartAttack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        canStartAttack = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isRunning = animator.GetBool("IsRunning");
+        bool forwardPressed = Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d");
+
+        if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            canStartAttack = false;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            canStartAttack = true;
+        }
+
+        if (canStartAttack)
+        {
+            animator.SetBool("IsMmaKick", true);
+        }
+        //bool isMmaKickTrue = animator.GetBool("IsMmaKick");
+
+        if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("Mma Kick"))
+        {
+            animator.SetBool("IsMmaKick", false);
+        }
+
+
+        if (!isRunning && forwardPressed)
+        {
+            animator.SetBool("IsRunning", true);
+        }
+
+        if (isRunning && !forwardPressed)
+        {
+            animator.SetBool("IsRunning", false);
+        }
+
+    }
+}
